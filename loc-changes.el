6e239c96542a6094bd08cf6e8e3d8dae90ffa0da
rocky <rocky@gnu.org>
2013-07-11 03:09:56 -0400

    Try travis

--- loc-changes.el
+++ loc-changes.el
@@ -23,15 +23,19 @@
 
 ;;; Commentary:
 
-;; The intent of this package is to let a user to set marks in a buffer prior
-;; to changes so that we can track the original positions after the change.
+;; This package lets users or programs set marks in a buffer prior to
+;; changes so that we can track the original positions after the
+;; change.
 
 ;; One common use is say when debugging a program.  The debugger has its static
 ;; notion of the file and positions inside that.  However it may be convenient
-;; for a programmer to edit the program but not change execution of the program.
+;; for a programmer to edit the program but not restart execution of the program.
 
 ;; Another use might be in a compilation buffer for errors and
 ;; warnings which refer to file and line positions.
+
+;; Should be available via Melpa.
+
 
 ;;; Code:
 
