f9b44b2f19a61f32fb319609c66a900c3c80132c
rocky <rocky@gnu.org>
2020-07-22 21:47:34 -0400

    Try to get TravisCi working again.

---
+++ run-travis-ci.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+set_default () {
+  eval "
+if [ -z \$$1 ]; then
+  $1=$2
+fi
+"
+}
+
+set_default EMACS "$(which emacs)"
+
+echo "*** Emacs version ***"
+echo "EMACS =" $(which $EMACS)
+$EMACS --version
+echo
+
+NO_CHECK_EMACS_PACKAGES=1 /bin/bash ./autogen.sh && cd test && make check
